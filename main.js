aaca0698390710aebe0aded6d43c8c69b10000c4
NikolozLekishvili <K0K0L0z1922@gmail.com>
2026-01-04 16:33:48 +0400

    2

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//reduce
+//prices by reduce
 const prices = [5,30,10,25,15,20];
 const total = prices.reduce(sum);
 
@@ -10,6 +10,21 @@ function sum(accumulator,element){
 
 }
 
+
+//grades by reduce
+
+const grades = [75,50,90,80,65,95]
+const max = grades.reduce(getmax)
+const min = grades.reduce(getmin)
+
+function getmax(accumulator,element){
+    return Math.max(accumulator,element)
+}
+function getmin(accumulator,element){
+    return Math.min(accumulator,element)
+}
+console.log(max)
+console.log(min)
 //random workout
 const num = [1,2,3,4,5]
 let  num1 = num.reduce(all,0)
